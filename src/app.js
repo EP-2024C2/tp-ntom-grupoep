756de626ddb9c491dcf240e8bbb65b44b7c07d3f
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2024-10-09 12:52:52 -0300

    Se agregaron valores iniciales a la semilla y se relacionaron entre ellos de n a m

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const routes = require('./routes')
-//const productoRuta = require('./routes/productos.route')
 const db = require('./models')
 const crearValoresInicialesDB = require('./seeders/semillaInicial')
 
--- src/seeders/semillaInicial.js
+++ src/seeders/semillaInicial.js
@@ -2,6 +2,18 @@ const {Producto, Fabricante, Componente} = require('../models')
 
 const crearValoresInicialesDB = async () => {
   try {
+    const fab1 = await Fabricante.create({
+      nombre: 'TechCorp',
+      numeroContacto: "+123456789",
+      direccion: "1234 Elm St, Ciudad",
+      pathImgPerfil: "/images/fabricantes/techcorp.jpg"
+    })
+    const fab2 = await Fabricante.create({
+      nombre: 'Innovatech',
+      numeroContacto: "+987654321",
+      direccion: "4567 Oak Ave, Ciudad",
+      pathImgPerfil: "/images/fabricantes/innovatech.jpg"
+    })
     const prod1 = await Producto.create({
       nombre: 'Laptop X200', 
       descripcion: 'Una laptop de alto rendimiento', 
@@ -32,13 +44,13 @@ const crearValoresInicialesDB = async () => {
       descripcion: 'Disco sólido de 1TB de capacidad'
     })
 
-    prod1.addComponentes([comp3, comp4])
-    /*
-    @TODO: terminar semilla
-    */
+    await fab1.addProductos([prod1, prod2])
+    await prod1.addComponentes([comp3, comp4])
+    await prod2.addComponentes([comp1, comp2])
+    await prod1.addFabricantes([fab1, fab2])
     
   } catch (error) {
-    console.log("no se pudo ☠")
+    console.log("☠ Error al crear los valores iniciales de la base de datos:", error)
   }
 }
 
