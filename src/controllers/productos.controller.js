3d238f6c3c79b1e0be9df9ff6d7df78b77657aa6
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2024-10-09 13:15:18 -0300

    formateo de codigo en productoController

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -2,32 +2,24 @@ const {Producto} = require('../models')
 
 const controller = {}
 
-const getAllProductos = async (req, res) => {
+controller.getAllProductos = async (req, res) => {
   const productos = await Producto.findAll({})
   res.status(200).json(productos)
 }
-controller.getAllProductos = getAllProductos
 
-const getProductoById= async (req, res)=>{
+
+controller.getProductoById = async (req, res)=>{
   const idProducto = req.params.id
   const producto = await Producto.findOne( {
       where: {id : idProducto}
   })
   res.status(200).json(producto)
 }
-controller.getProductoById = getProductoById
 
-const createProducto = async(req,res) => {
+controller.createProducto = async(req,res) => {
   const {nombre,descripcion,precio,pathImg} = req.body
-  const producto = await Producto.create({
-    nombre : nombre,                        // Se le puede saca el value ya que es lo mismo porque llevan el mismo nombre, lo dejo por si acaso
-    descripcion : descripcion,
-    precio : precio,
-    pathImg : pathImg
-  })
+  const producto = await Producto.create({ nombre, descripcion, precio, pathImg })
   res.status(201).json(producto)
 }
-
-controller.createProducto = createProducto
 
 module.exports = controller
